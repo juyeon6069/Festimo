8bd90d947a2cf2975bee1af44448158519c455d2
juyeon6069 <jynam6069@gmail.com>
2025-01-23 16:30:52 +0900

    ♻️ refactor : festival repository 마이그레이션

--- src/main/java/com/example/festimo/domain/festival/repository/FestivalRepository.kt
+++ src/main/java/com/example/festimo/domain/festival/repository/FestivalRepository.kt
@@ -1,24 +1,26 @@
-package com.example.festimo.domain.festival.repository;
+package com.example.festimo.domain.festival.repository
 
-import com.example.festimo.domain.festival.domain.Festival;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-
-import java.time.LocalDate;
-import java.util.List;
+import com.example.festimo.domain.festival.domain.Festival
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import org.springframework.stereotype.Repository
+import java.time.LocalDate
 
 @Repository
-public interface FestivalRepository extends JpaRepository<Festival, String> {
-    Page<Festival> findByTitleContainingIgnoreCase(String keyword, Pageable pageable);
-    Page<Festival> findByAddressContainingIgnoreCase(String region, Pageable pageable);
+interface FestivalRepository : JpaRepository<Festival, String> {
+
+    fun findByTitleContainingIgnoreCase(keyword: String?, pageable: Pageable?): Page<Festival>
+
+    fun findByAddressContainingIgnoreCase(region: String?, pageable: Pageable?): Page<Festival>
 
     @Query("SELECT f FROM Festival f WHERE " +
             "f.startDate <= :endDate AND f.endDate >= :startDate")
-    Page<Festival> findByMonth(@Param("startDate") LocalDate startDate,
-                               @Param("endDate") LocalDate endDate,
-                               Pageable pageable);
+    fun findByMonth(
+        @Param("startDate") startDate: LocalDate?,
+        @Param("endDate") endDate: LocalDate?,
+        pageable: Pageable?
+    ): Page<Festival>
 }
