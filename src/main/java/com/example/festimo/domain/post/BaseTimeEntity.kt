6a92544891e344718a995ee60bbef06fcc2678f1
exxyeon <leehayeon200@naver.com>
2025-01-24 10:43:14 +0900

    :recycle: refactor: BaseTimeEntity 코틀린으로 마이그레이션

--- src/main/java/com/example/festimo/domain/post/BaseTimeEntity.kt
+++ src/main/java/com/example/festimo/domain/post/BaseTimeEntity.kt
@@ -1,34 +1,32 @@
-package com.example.festimo.domain.post;
+package com.example.festimo.domain.post
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.PrePersist;
-import lombok.Getter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import jakarta.persistence.PrePersist
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
 
-import java.time.LocalDateTime;
-
-@Getter
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
-public abstract class BaseTimeEntity {
-
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseTimeEntity {
     @CreatedDate
     @Column(name = "created_at", updatable = false)
-    private LocalDateTime createdAt;
+    var createdAt: LocalDateTime? = null
+        protected set
 
     @LastModifiedDate
     @Column(name = "updated_at", insertable = false)
-    private LocalDateTime updatedAt;
+    var updatedAt: LocalDateTime? = null
+        protected set
 
     @PrePersist
-    public void prePersist() {
-        if (this.createdAt == null) {
-            this.createdAt = LocalDateTime.now();
+    fun prePersist() {
+        if (createdAt == null) {
+            createdAt = LocalDateTime.now()
         }
-        this.updatedAt = null;
+        updatedAt = null
     }
 }
