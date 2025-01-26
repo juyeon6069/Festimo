5d001d97cd8d5d9f031713c3063e1496c4db0172
exxyeon <leehayeon200@naver.com>
2025-01-26 18:09:17 +0900

    :recycle: refactor: scheduler 코틀린으로 마이그레이션

--- src/main/java/com/example/festimo/domain/post/scheduler/WeeklyTopPostScheduler.kt
+++ src/main/java/com/example/festimo/domain/post/scheduler/WeeklyTopPostScheduler.kt
@@ -1,28 +1,24 @@
-package com.example.festimo.domain.post.scheduler;
+package com.example.festimo.domain.post.scheduler
 
-import com.example.festimo.domain.post.service.PostService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
+import com.example.festimo.domain.post.service.PostService
+import org.slf4j.LoggerFactory
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
 
-@Slf4j
 @Component
-@RequiredArgsConstructor
 @EnableScheduling
-public class WeeklyTopPostScheduler {
+class WeeklyTopPostScheduler(private val postService: PostService) {
+    private val logger = LoggerFactory.getLogger(javaClass)
 
-    private final PostService postService;
-
-    @Scheduled(fixedRate = 3600000) // 1시간마다 실행
-    public void updateWeeklyTopPosts() {
+    @Scheduled(fixedRate = 3600000)
+    fun updateWeeklyTopPosts() {
         try {
-            postService.clearWeeklyTopPostsCache();
-            postService.getCachedWeeklyTopPosts();
-            log.info("주간 인기 게시글 캐시를 성공적으로 갱신했습니다.");
-        } catch (Exception e) {
-            log.error("주간 인기 게시글 캐시 갱신에 실패했습니다.", e);
+            postService.clearWeeklyTopPostsCache()
+            postService.getCachedWeeklyTopPosts()
+            logger.info("주간 인기 게시글 캐시를 성공적으로 갱신했습니다.")
+        } catch (e: Exception) {
+            logger.error("주간 인기 게시글 캐시 갱신에 실패했습니다.", e)
         }
     }
 }
